e6066605e5a64ea5e8bf9f0069ed15c754198c7b
GavMan1995 <gavyncaldwell@gmail.com>
2016-05-06 20:32:46 -0600

    doesnt let you import excel, dont know why, only csv until i figure out why

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,20 +1,7 @@
 class Home < ActiveRecord::Base
   def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      home = find_by_id(row["id"]) || new
-      home.save!
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
-    when ".xls" then Roo::Excel.new(file.path, extension: :xls)
-    when ".xlsx" then Roo::Excelx.new(file.path, extension: :xlsx)
-    else raise "Unknown file type: #{file.original_filename}"
+    CSV.foreach(file.path, headers: true) do |row|
+      Home.create! row.to_hash
     end
   end
 end
