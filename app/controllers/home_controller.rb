89550c2b431ee27f5f26365d9c7165975b9922c9
GavMan1995 <gavyncaldwell@gmail.com>
2016-05-11 14:26:40 -0600

    added csv functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,28 @@ require 'csv'
 class HomeController < ApplicationController
   before_action :rillow
   def index
+    @homes = Home.all
   end
 
 
   def import
     Home.import(params[:file])
+    homes = Home.all
+    homes.each do |home|
+      address = home.propad
+      city = home.prop_city
+      state = home.prop_state
+      zillow = @rillow.get_search_results("#{address}", "#{city}, #{state}")
+      zillow.to_hash
+      zestimate = zillow.find_attribute 'zestimate'
+      if  zestimate == nil
+        zestimate = "no zestimate available"
+      elsif zestimate != nil
+        zestimate = zillow.find_attribute 'zestimate'
+        zestimate = zestimate.first.first[1].first["content"].to_i
+      end
+      Home.update(home.id, zestimate: zestimate)
+    end
     redirect_to root_url, notice: "Products imported."
   end
 
